9582c7d555c7b53280c11b85b097d1006ce37bce
Sidiqi <qayum.sidiqi@gmail.com>
2026-09-16 19:19:28 +0000

    feat(network): implement network security controls (#20)

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -41,3 +41,18 @@ output "private_route_table_id" {
   description = "ID of the private route table."
   value       = aws_route_table.private.id
 }
+
+output "alb_security_group_id" {
+  description = "ID of the ALB security group."
+  value       = aws_security_group.alb.id
+}
+
+output "application_security_group_id" {
+  description = "ID of the application workload security group."
+  value       = aws_security_group.application.id
+}
+
+output "database_security_group_id" {
+  description = "ID of the database security group."
+  value       = aws_security_group.database.id
+}
