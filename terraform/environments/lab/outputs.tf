d0bca50180af5818f8675c59ed8d82fc51077c32
Sidiqi <qayum.sidiqi@gmail.com>
2026-09-16 18:40:19 +0000

    feat(iam): implement EC2 workload identity (#14)

--- terraform/environments/lab/outputs.tf
+++ terraform/environments/lab/outputs.tf
@@ -27,3 +27,18 @@ output "administrative_role_arn" {
   description = "ARN of the controlled administrative role."
   value       = module.iam.administrative_role_arn
 }
+
+output "ec2_workload_role_name" {
+  description = "Name of the EC2 workload IAM role."
+  value       = module.iam.ec2_workload_role_name
+}
+
+output "ec2_workload_role_arn" {
+  description = "ARN of the EC2 workload IAM role."
+  value       = module.iam.ec2_workload_role_arn
+}
+
+output "ec2_workload_instance_profile_name" {
+  description = "Name of the EC2 workload instance profile."
+  value       = module.iam.ec2_workload_instance_profile_name
+}
