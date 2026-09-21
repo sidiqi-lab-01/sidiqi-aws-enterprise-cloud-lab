425560c95458af450153dd8b9a6be6ae86e15a50
Sidiqi <qayum.sidiqi@gmail.com>
2026-09-21 19:44:02 +0000

    Record public content cleanup deployment

--- terraform/environments/lab/compute.tf
+++ terraform/environments/lab/compute.tf
@@ -16,6 +16,6 @@ module "compute" {
   instance_profile_name         = module.iam.ec2_workload_instance_profile_name
   target_group_arn              = module.network.application_target_group_arn
 
-  application_image   = "785823823915.dkr.ecr.us-east-1.amazonaws.com/sidiqi-aws-enterprise-cloud-lab-lab-application@sha256:b97edf2c464a238e8edd6e621a9b452818377722686896db7c31229b09f183da"
-  application_version = "web-946545a"
+  application_image   = "785823823915.dkr.ecr.us-east-1.amazonaws.com/sidiqi-aws-enterprise-cloud-lab-lab-application@sha256:ac5e03b427263a944812b3fa15f8d8a7a023d5d0c26959a595c7ecf62a199ebf"
+  application_version = "web-7353f60"
 }
